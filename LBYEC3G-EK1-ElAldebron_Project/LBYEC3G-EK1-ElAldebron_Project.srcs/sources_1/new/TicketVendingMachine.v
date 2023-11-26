2d3edae1e379211eb39c21987ef393520950d1b0
Keane Dwight Sulit <88814054+keane-sulit@users.noreply.github.com>
2023-11-27 01:53:44 +0800

    Added initial files

---
+++ LBYEC3G-EK1-ElAldebron_Project/LBYEC3G-EK1-ElAldebron_Project.srcs/sources_1/new/TicketVendingMachine.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Bettina Dayrit, Gia Guevarra, Randell Lariza, Mariah Rodriguez, Keane Sulit
+// 
+// Create Date: 27.11.2023 01:52:20
+// Design Name: 
+// Module Name: TicketVendingMachine
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TicketVendingMachine(
+
+    );
+endmodule
